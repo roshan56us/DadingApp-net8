9a143a6be475ad58433cefdb27ff94b274bb86e2
Dharmendra Kumar <roshan56us@yahoo.com>
2025-03-15 14:36:36 +0530

    End of section 3

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -26,7 +26,8 @@ var app = builder.Build();
 // app.UseHttpsRedirection();
 
 // app.UseAuthorization();
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
