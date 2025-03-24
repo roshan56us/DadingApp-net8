67a4e98b337224ba0f1fd32ea3fdc7d5cbf66ce6
Dharmendra Kumar <roshan56us@yahoo.com>
2025-03-24 19:41:31 +0530

    Before using signal

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  @Input() usersFromHomeComponent: any;
+model: any={}
+register(){
+  console.log(this.model);
+}
+cancel()
+{
+  console.log('cancelled');
+}
+}
